0e4b258a65cdcace0e841deb9c0f24d778545c68
Pedro Falfan <pe.falfan@duocuc.cl>
2024-03-21 21:50:12 -0300

    First commit, base project

---
+++ src/main/java/cl/semana3_a/pedro_falfan/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cl.semana3_a.pedro_falfan;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PedroFalfanApplication.class);
+	}
+
+}
---
+++ src/test/java/cl/semana3_a/pedro_falfan/PedroFalfanApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.semana3_a.pedro_falfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedroFalfanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
